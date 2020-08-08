0c25029ef0f8bfc053d9ca32779805dcbb1b3f42
Charles McCarthy <charles@studybundles.com>
2020-08-08 17:19:25 +0100

    Setup Project Details

--- services/api-shared-modules/src/models/DynamoDBItem.ts
+++ services/api-shared-modules/src/models/DynamoDBItem.ts
@@ -9,7 +9,7 @@ export interface DynamoDbItem {
 }
 
 // TODO: Implement a mechanism to change this automatically based on environment
-@table('TestProject-GENERAL')
+@table('CryptoTracker-GENERAL')
 export class DynamoDbItem implements DynamoDbItem {
 	@hashKey()
 	public pk!: string;
