fcb170ff056af2f086d175adb3fc2c6d0753f744
Charles McCarthy <charles@studybundles.com>
2020-10-17 16:46:25 +0100

    External Http Post Requests

--- services/api-shared-modules/src/external-apis/http-api.ts
+++ services/api-shared-modules/src/external-apis/http-api.ts
@@ -1,5 +1,6 @@
 import { ClientRequest, IncomingMessage } from 'http';
 import * as https from 'https';
+import { BINANCE_API_HOST } from '../../../../environment/env';
 
 export class HttpApi {
 
@@ -8,6 +9,25 @@ export class HttpApi {
 			let dataString: string = '';
 
 			const req: ClientRequest = https.get(url, { headers }, (res: IncomingMessage) => {
+				res.on('data', (chunk: any) => dataString += chunk);
+				res.on('end', () => resolve(dataString));
+			});
+
+			req.on('error', reject);
+		});
+	}
+
+	public static async post(url: string, headers?: { [key: string]: string }): Promise<string> {
+		return new Promise((resolve: any, reject: any): void => {
+			let dataString: string = '';
+
+			const req: ClientRequest = https.get({ // https.request not working - Using https.get with POST method (It works?)
+				host: BINANCE_API_HOST,
+				port: 443,
+				path: url,
+				method: 'POST',
+				headers
+			}, (res: IncomingMessage) => {
 				res.on('data', (chunk: any) => dataString += chunk);
 				res.on('end', () => resolve(dataString));
 			});
