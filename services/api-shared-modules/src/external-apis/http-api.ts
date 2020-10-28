3dbe00b13e6f950cb5897f9f4e655e86acd88998
Charles McCarthy <charles@studybundles.com>
2020-10-28 20:20:27 +0000

    Added PUT Requests To External API Caller

--- services/api-shared-modules/src/external-apis/http-api.ts
+++ services/api-shared-modules/src/external-apis/http-api.ts
@@ -1,6 +1,5 @@
 import { ClientRequest, IncomingMessage } from 'http';
 import * as https from 'https';
-import { BINANCE_API_HOST } from '../../../../environment/env';
 
 export class HttpApi {
 
@@ -17,15 +16,34 @@ export class HttpApi {
 		});
 	}
 
-	public static async post(url: string, headers?: { [key: string]: string }): Promise<string> {
+	public static async post(url: string, host: string, secureHttp: boolean, headers?: { [key: string]: string }): Promise<string> {
 		return new Promise((resolve: any, reject: any): void => {
 			let dataString: string = '';
 
 			const req: ClientRequest = https.get({ // https.request not working - Using https.get with POST method (It works?)
-				host: BINANCE_API_HOST,
-				port: 443,
+				host,
+				port: secureHttp ? 443 : 80,
 				path: url,
 				method: 'POST',
+				headers
+			}, (res: IncomingMessage) => {
+				res.on('data', (chunk: any) => dataString += chunk);
+				res.on('end', () => resolve(dataString));
+			});
+
+			req.on('error', reject);
+		});
+	}
+
+	public static async put(url: string, host: string, secureHttp: boolean, headers?: { [key: string]: string }): Promise<string> {
+		return new Promise((resolve: any, reject: any): void => {
+			let dataString: string = '';
+
+			const req: ClientRequest = https.get({ // https.request not working - Using https.get with POST method (It works?)
+				host,
+				port: secureHttp ? 443 : 80,
+				path: url,
+				method: 'PUT',
 				headers
 			}, (res: IncomingMessage) => {
 				res.on('data', (chunk: any) => dataString += chunk);
