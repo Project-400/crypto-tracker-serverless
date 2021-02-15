95e70cfa08c7ce1139bec8b7a6b401b508d6da19
Charles McCarthy <charles@studybundles.com>
2021-02-15 20:49:24 +0000

    Get Wallet Valuation

--- services/api-shared-modules/src/data-access/interfaces/IWalletValuationRepository.ts
+++ services/api-shared-modules/src/data-access/interfaces/IWalletValuationRepository.ts
@@ -1,6 +1,7 @@
-import { WalletValuation } from '@crypto-tracker/common-types';
+import { WalletValuation, WalletValue } from '@crypto-tracker/common-types';
 
 export interface IWalletValuationRepository {
-	create(userId: string, value: string): Promise<WalletValuation>;
-	update(walletValuationId: string, userId: string, value: string, changes: Partial<WalletValuation>): Promise<WalletValuation>;
+	get(userId: string, roundedTime: string): Promise<WalletValuation>;
+	create(userId: string, roundedHour: string, roundedMinute: string, walletValue: WalletValue): Promise<WalletValuation>;
+	update(roundedTime: string, userId: string, changes: Partial<WalletValuation>): Promise<WalletValuation>;
 }
--- services/api-shared-modules/src/data-access/repositories/WalletValuationRepository.ts
+++ services/api-shared-modules/src/data-access/repositories/WalletValuationRepository.ts
@@ -1,41 +1,48 @@
 import { IWalletValuationRepository } from '../interfaces/IWalletValuationRepository';
 import { WalletValuationItem } from '../../models/core/WalletValuation';
-import { v4 as uuid } from 'uuid';
 import { WalletValuation, WalletValue } from '@crypto-tracker/common-types';
+import { EntitySortType } from '../../types/entity-sort-types';
+import { Repository } from './Repository';
+import { Entity } from '../../types/entities';
 
 export class WalletValuationRepository extends Repository implements IWalletValuationRepository {
 
-	public async create(userId: string, value: string): Promise<WalletValuation> {
-		const walletValuationId: string = uuid();
-		const millis: number = 1000 * 60;
-		const date: Date = new Date();
-		const dateISO: string = date.toISOString();
-		const roundedTime: string = new Date(Math.ceil(date.getTime() / millis) * millis).toISOString();
-		const walletValue: WalletValue = {
-			value,
-			time: roundedTime
-		};
+	public async get(userId: string, roundedTime: string): Promise<WalletValuation> {
+		try {
+			return await this.db.get(Object.assign(new WalletValuationItem(), {
+				pk: `${Entity.WALLET_VALUATION}#${roundedTime}`,
+				sk: `${Entity.USER}#${userId}/${EntitySortType.TIME}#${roundedTime}`
+			}));
+		} catch (e) {
+			return undefined;
+		}
+	}
+
+	public async create(userId: string, roundedHour: string, roundedMinute: string, walletValue: WalletValue): Promise<WalletValuation> {
+		const date: string = new Date().toISOString();
 
 		return this.db.put(Object.assign(new WalletValuationItem(), {
-			pk: `${Entity.WALLET_VALUATION}#${walletValuationId}`,
-			sk: `${Entity.USER}#${userId}/`,
-			sk2: `${Entity.WALLET_VALUATION}#${roundedTime}`,
+			pk: `${Entity.WALLET_VALUATION}#${roundedHour}`,
+			sk: `${Entity.USER}#${userId}/${EntitySortType.TIME}#${roundedHour}`,
+			sk2: `${Entity.WALLET_VALUATION}#${roundedHour}`,
 			entity: Entity.WALLET_VALUATION,
 			times: {
-				createdAt: dateISO,
-				valueStartingAt: roundedTime
+				createdAt: date,
+				valueStartingAt: roundedMinute
 			},
 			values: [ walletValue ]
 		}));
 	}
 
-	public async update(walletValuationId: string, userId: string, value: string, changes: Partial<WalletValuation>):
+	public async update(roundedHour: string, userId: string, changes: Partial<WalletValuation>):
 		Promise<WalletValuation> {
 		delete changes.sk2;
 		delete changes.sk3;
 
+		changes.times.updatedAt = new Date().toISOString();
+
 		return this.db.update(Object.assign(new WalletValuationItem(), {
-			pk: `${Entity.WALLET_VALUATION}#${walletValuationId}`,
+			pk: `${Entity.WALLET_VALUATION}#${roundedHour}`,
 			sk: `${Entity.USER}#${userId}/`,
 			...changes
 		}), {
