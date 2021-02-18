5febc53cfeac3c2c3f4fa0723f51c124469d3f9a
Charles McCarthy <charles@studybundles.com>
2021-02-18 20:30:22 +0000

    Get Single Kline Values Query

--- services/api-shared-modules/src/data-access/interfaces/IKlineValuesRepository.ts
+++ services/api-shared-modules/src/data-access/interfaces/IKlineValuesRepository.ts
@@ -1,7 +1,7 @@
-import { KlineValues } from '@crypto-tracker/common-types';
+import { KlineValues, VALUE_LOG_INTERVAL } from '@crypto-tracker/common-types';
 
 export interface IKlineValuesRepository {
-	// get(userId: string, interval: VALUE_LOG_INTERVAL, roundedTime: string): Promise<KlineValues>;
+	get(userId: string, interval: VALUE_LOG_INTERVAL, roundedTime: string): Promise<KlineValues>;
 	// getRange(userId: string, interval: VALUE_LOG_INTERVAL, roundedTime: string): Promise<KlineValues[]>;
 	create(userId: string, walletValue: Partial<KlineValues>): Promise<KlineValues>;
 	// update(userId: string, changes: Partial<KlineValues>): Promise<KlineValues>;
--- services/api-shared-modules/src/data-access/repositories/KlineValuesRepository.ts
+++ services/api-shared-modules/src/data-access/repositories/KlineValuesRepository.ts
@@ -1,4 +1,4 @@
-import { KlineValues } from '@crypto-tracker/common-types';
+import { KlineValues, VALUE_LOG_INTERVAL } from '@crypto-tracker/common-types';
 import { EntitySortType } from '../../types/entity-sort-types';
 import { Repository } from './Repository';
 import { Entity } from '../../types/entities';
@@ -7,16 +7,16 @@ import { KlineValuesItem } from '../../models/core/KlineValues';
 
 export class KlineValuesRepository extends Repository implements IKlineValuesRepository {
 
-	// public async get(userId: string, interval: VALUE_LOG_INTERVAL, roundedTime: string): Promise<KlineValues> {
-	// 	try {
-	// 		return await this.db.get(Object.assign(new KlineValuesItem(), {
-	// 			pk: `${Entity.WALLET_VALUATION}#${roundedTime}`,
-	// 			sk: `${Entity.USER}#${userId}/${EntitySortType.INTERVAL}#${interval}/${EntitySortType.TIME}#${roundedTime}`
-	// 		}));
-	// 	} catch (e) {
-	// 		return undefined;
-	// 	}
-	// }
+	public async get(userId: string, interval: VALUE_LOG_INTERVAL, roundedTime: string): Promise<KlineValues> {
+		try {
+			return await this.db.get(Object.assign(new KlineValuesItem(), {
+				pk: `${Entity.KLINE_VALUES}#${roundedTime}`,
+				sk: `${Entity.USER}#${userId}/${EntitySortType.INTERVAL}#${interval}/${EntitySortType.TIME}#${roundedTime}`
+			}));
+		} catch (e) {
+			return undefined;
+		}
+	}
 
 	public async create(userId: string, klineValues: Partial<KlineValues>): Promise<KlineValues> {
 		const date: string = new Date().toISOString();
