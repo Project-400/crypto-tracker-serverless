aca447790a94f6d4deb6ade100c5f11495728528
Charles McCarthy <charles@studybundles.com>
2021-02-19 12:57:31 +0000

    Valuation Get Mocks

---
+++ services/api-valuation/tests/mock.ts
@@ -0,0 +1,18 @@
+// // export const mockPlaySoundFile = jest.fn();
+// import Mock = jest.Mock;
+//
+// const mock: Mock = jest.fn().mockImplementation(() => {
+// 	return {
+// 		WalletValuation: {
+// 			get: (userId, interval, time) => {
+// 				console.log("INSIDE TEST");
+// 				console.log(userId);
+// 				console.log(interval);
+// 				console.log(time);
+// 			},
+// 			create: jest.fn()
+// 		}
+// 	};
+// });
+//
+// export default mock;
---
+++ services/api-valuation/tests/mocks.ts
@@ -0,0 +1,14 @@
+import { VALUE_LOG_INTERVAL, WalletValuation } from '@crypto-tracker/common-types';
+
+export const WalletValuation_GetExistingMinuteLog: WalletValuation = {
+	pk: '',
+	sk: '',
+	sk2: '',
+	entity: '',
+	value: '500',
+	time: '2021-02-19T11:16:00.000Z',
+	interval: VALUE_LOG_INTERVAL.MINUTE,
+	times: {
+		createdAt: '2021-02-19T11:16:25.000Z'
+	}
+};
