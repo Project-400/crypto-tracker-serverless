874569f5f6de6f04552067c77b78a1f657b55a76
Charles McCarthy <charles@studybundles.com>
2021-02-19 13:51:14 +0000

    Updated Date Utils

--- services/api-shared-modules/src/utils/datetime.ts
+++ services/api-shared-modules/src/utils/datetime.ts
@@ -2,6 +2,16 @@ import moment from 'moment';
 
 export default class DateTimeFunctions {
 
+	private static minuteMillis: number = 1000 * 60;
+	private static hourMillis: number = DateTimeFunctions.minuteMillis * 60;
+	private static dayMillis: number = DateTimeFunctions.hourMillis * 24;
+
 	public static IsWithinHours = (hours: number, date: Date | string): boolean => moment(date) < moment().subtract(hours, 'hours');
+
+	public static FloorTime = (millis: number): string => new Date(Math.floor(new Date().getTime() / millis) * millis).toISOString();
+
+	public static FloorMinute: string = DateTimeFunctions.FloorTime(DateTimeFunctions.minuteMillis);
+	public static FloorHour: string  = DateTimeFunctions.FloorTime(DateTimeFunctions.hourMillis);
+	public static FloorDay: string  = DateTimeFunctions.FloorTime(DateTimeFunctions.dayMillis);
 
 }
