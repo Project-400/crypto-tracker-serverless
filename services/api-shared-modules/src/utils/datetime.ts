75b71db4bd9b4a5b67f3dabbd94bfc510a44f300
Charles McCarthy <charles@studybundles.com>
2021-02-27 17:54:40 +0000

    Added New Datetime Util Functions

--- services/api-shared-modules/src/utils/datetime.ts
+++ services/api-shared-modules/src/utils/datetime.ts
@@ -17,5 +17,8 @@ export class DatetimeUtils {
 	public FloorMinute = (): string => this.FloorTime(this.minuteMillis);
 	public FloorHour = (): string => this.FloorTime(this.hourMillis);
 	public FloorDay = (): string => this.FloorTime(this.dayMillis);
+	public PreviousFloorMinute = (count: number): string => this.FloorTime(this.minuteMillis * count);
+	public PreviousFloorHour = (count: number): string => this.FloorTime(this.hourMillis * count);
+	public PreviousFloorDay = (count: number): string => this.FloorTime(this.dayMillis * count);
 
 }
