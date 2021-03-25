e50a696e9cd66edbc27732efe8f8c8fcdd1b51f2
Charles McCarthy <charles@studybundles.com>
2021-03-25 20:35:36 +0000

    Return Success To CodePipeline

--- services/api-ec2-deployments/src/ec2-deployments.controller.ts
+++ services/api-ec2-deployments/src/ec2-deployments.controller.ts
@@ -1,27 +1,32 @@
-import { ApiContext, ApiEvent, ApiHandler, ApiResponse, ErrorCode, ResponseBuilder } from '../../api-shared-modules/src';
+import { ApiContext, ApiEvent, ApiHandler } from '../../api-shared-modules/src';
 import { Ec2DeploymentsService } from './ec2-deployments.service';
 import { CodePipeline } from 'aws-sdk';
+
+const codePipeline: CodePipeline = new CodePipeline();
 
 export class Ec2DeploymentsController {
 
 	public constructor(private ec2DeploymentsService: Ec2DeploymentsService) { }
 
-	public addLatestDeployment: ApiHandler = async (event: ApiEvent, context: ApiContext): Promise<ApiResponse> => {
-		// const auth: TokenVerification = Auth.VerifyToken('');
-		// const userId: string = auth.sub;
-
-		console.log(event);
-
-		if (!event['CodePipeline.job']) return ResponseBuilder.badRequest(ErrorCode.BadRequest, 'CodePipeline Job Missing');
+	public addLatestDeployment: ApiHandler = async (event: ApiEvent, context: ApiContext): Promise<any> => {
+		if (!event['CodePipeline.job']) return codePipeline.putJobFailureResult().promise();
 
 		try {
 			const codePipelineJob: CodePipeline.Job = event['CodePipeline.job'];
 
 			const updated: boolean = await this.ec2DeploymentsService.updateLatestDeployment(codePipelineJob);
 
-			return ResponseBuilder.ok({ event, updated });
+			if (updated) {
+				const params: { jobId: string } = {
+					jobId: codePipelineJob.id
+				};
+
+				return codePipeline.putJobSuccessResult(params).promise();
+			}
+
+			return codePipeline.putJobFailureResult().promise();
 		} catch (err) {
-			return ResponseBuilder.internalServerError(err, err.message);
+			return codePipeline.putJobFailureResult().promise();
 		}
 	}
 
