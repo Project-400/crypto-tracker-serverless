c1dfffd03ded789ac701803c9248e9db3ce544c3
Charles McCarthy <charles@studybundles.com>
2021-03-25 18:46:00 +0000

    Receive CodePipeline Job

--- services/api-ec2-deployments/src/ec2-deployments.controller.ts
+++ services/api-ec2-deployments/src/ec2-deployments.controller.ts
@@ -1,20 +1,25 @@
-import { ApiContext, ApiEvent, ApiHandler, ApiResponse, ErrorCode, ResponseBuilder } from '../../api-shared-modules/src';
+import { ApiContext, ApiEvent, ApiHandler, ApiResponse, ResponseBuilder } from '../../api-shared-modules/src';
 import Auth, { TokenVerification } from '../../_auth/verify';
+import { Ec2DeploymentsService } from './ec2-deployments.service';
+import { CodePipeline } from 'aws-sdk';
 
 export class Ec2DeploymentsController {
 
-	public constructor() { }
+	public constructor(private ec2DeploymentsService: Ec2DeploymentsService) { }
 
-	public retrieveValuationLog: ApiHandler = async (event: ApiEvent, context: ApiContext): Promise<ApiResponse> => { // old data
-		if (!event.pathParameters || !event.pathParameters.time)
-			return ResponseBuilder.badRequest(ErrorCode.BadRequest, 'Invalid request parameters');
+	public updateLatestDeployment: ApiHandler = async (event: ApiEvent, context: ApiContext): Promise<ApiResponse> => { // old data
+		// if (!event.pathParameters || !event.pathParameters.time)
+		// 	return ResponseBuilder.badRequest(ErrorCode.BadRequest, 'Invalid request parameters');
 
 		const auth: TokenVerification = Auth.VerifyToken('');
 		const userId: string = auth.sub;
 
 		try {
+			const codePipelineJob: CodePipeline.Job = event['CodePipeline.job'];
 
-			return ResponseBuilder.ok({ log });
+			const updated: boolean = await this.ec2DeploymentsService.updateLatestDeployment(codePipelineJob);
+
+			return ResponseBuilder.ok({ event });
 		} catch (err) {
 			return ResponseBuilder.internalServerError(err, err.message);
 		}
--- services/api-ec2-deployments/src/handler.ts
+++ services/api-ec2-deployments/src/handler.ts
@@ -1,17 +1,9 @@
 import { ApiHandler, UnitOfWork } from '../../api-shared-modules/src';
 import { Ec2DeploymentsController } from './ec2-deployments.controller';
 import { Ec2DeploymentsService } from './ec2-deployments.service';
-import { ExchangeInfoService } from '../../api-exchange-info/src/exchange-info.service';
-import { CoinsService } from '../../api-coins/src/coins.service';
-import { WalletValuationService } from './wallet-valuation.service';
-import { DatetimeUtils } from '../../api-shared-modules/src/utils/datetime';
 
 const unitOfWork: UnitOfWork = new UnitOfWork();
-const exchangeInfoService: ExchangeInfoService = new ExchangeInfoService(unitOfWork);
-const coinsService: CoinsService = new CoinsService(unitOfWork);
-const datetimeUtils: DatetimeUtils = new DatetimeUtils();
-const walletValuationService: WalletValuationService = new WalletValuationService(unitOfWork, datetimeUtils);
-const valuationService: Ec2DeploymentsService = new Ec2DeploymentsService(exchangeInfoService);
-const controller: Ec2DeploymentsController = new Ec2DeploymentsController(valuationService, coinsService, walletValuationService);
+const ec2DeploymentsService: Ec2DeploymentsService = new Ec2DeploymentsService(unitOfWork);
+const controller: Ec2DeploymentsController = new Ec2DeploymentsController(ec2DeploymentsService);
 
-export const retrieveValuationLog: ApiHandler = controller.retrieveValuationLog;
+export const updateLatestDeployment: ApiHandler = controller.updateLatestDeployment;
